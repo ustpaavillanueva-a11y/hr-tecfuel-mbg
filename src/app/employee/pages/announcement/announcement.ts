6e9e7ef326c5055abc0f47cff24e01a3227a4289
Jerick <ustpaa.villanueva@ustp.edu.ph>
2026-04-22 14:31:46 +0800

    feat: remove announcement component and related files

--- src/app/employee/pages/announcement/announcement.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-announcement',
-  imports: [],
-  templateUrl: './announcement.html',
-  styleUrl: './announcement.scss',
-})
-export class Announcement {}
